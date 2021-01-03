b25ebb2341302ad1775a26c38248f99932ee36c6
paul <paul.butcher@gmail.com>
2021-01-03 13:41:23 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/paul-butcher/light_character',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
